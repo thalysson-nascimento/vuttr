8df89cac3bf05d82600ffcea952d58e807164f04
Thalysson <thalyssonwg.nascimento@gmail.com>
2020-02-11 13:08:08 -0300

    Creating SessionController

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const router = new Router();
 
 router.post('/users', UserController.store);
+
+router.post('/sessions', SessionController.store);
 
 export default router;
