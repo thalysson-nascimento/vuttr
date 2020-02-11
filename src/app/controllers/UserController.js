171cedab1a5634ac6158d957c40e8d1e183277fa
Thalysson <thalyssonwg.nascimento@gmail.com>
2020-02-11 12:36:47 -0300

    Validation UserController store

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string()
+                .required()
+                .min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res
+                .status(400)
+                .json({ error: 'Erro na validação de dados' });
+        }
+
         const userExist = await User.findOne({
             where: { email: req.body.email },
         });
