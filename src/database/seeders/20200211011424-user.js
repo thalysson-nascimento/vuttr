401c519d4dceb6312da54dfda2068d8987822ca1
Thalysson <thalyssonwg.nascimento@gmail.com>
2020-03-10 18:11:06 -0300

    update text to VUTTR

--- src/database/seeders/20200211011424-user.js
+++ src/database/seeders/20200211011424-user.js
@@ -6,7 +6,7 @@ module.exports = {
             'users',
             [
                 {
-                    name: 'Distribuidora FastFeet',
+                    name: 'Distribuidora VUTTR',
                     email: 'admin@vuttr.com',
                     password_hash: bcrypt.hashSync('123456', 8),
                     created_at: new Date(),
